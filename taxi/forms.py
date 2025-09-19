8c405ffafec3870acb7de1d25c750770cf7d317d
Bartolomeu <bartoszokroj@wp.pl>
2025-09-19 19:26:19 +0200

    solution

---
+++ taxi/forms.py
@@ -0,0 +1,50 @@
+import re
+
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from django.forms import ModelForm, ModelMultipleChoiceField, CheckboxSelectMultiple
+from django.contrib.auth.forms import UserCreationForm
+from django.forms.formsets import formset_factory
+
+from .models import Driver, Car
+
+pat = re.compile(r'^[A-Z]{3}[0-9]{5}$')
+
+class CarForm(ModelForm):
+    drivers = ModelMultipleChoiceField(queryset=get_user_model().objects.all(), widget=CheckboxSelectMultiple)
+
+    class Meta:
+        model = Car
+        fields = "__all__"
+
+
+class DriverCreationForm(UserCreationForm):
+
+    class Meta(UserCreationForm.Meta):
+        model = Driver
+        fields = UserCreationForm.Meta.fields + ("license_number",)
+
+
+    def clean_license_number(self):
+        license_number = self.cleaned_data["license_number"]
+
+        if pat.fullmatch(license_number):
+            return license_number
+
+        raise ValidationError("The format should be: 3 uppercase letters + 5 digits")
+
+
+class DriverUpdateForm(ModelForm):
+
+    class Meta:
+        model = Driver
+        fields = "license_number",
+
+
+    def clean_license_number(self):
+        license_number = self.cleaned_data["license_number"]
+
+        if pat.fullmatch(license_number):
+            return license_number
+
+        raise ValidationError("The format should be: 3 uppercase letters + 5 digits")
--- taxi/views.py
+++ taxi/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.http import HttpResponseNotAllowed
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from .forms import DriverCreationForm, DriverUpdateForm, CarForm
 from .models import Driver, Car, Manufacturer
 
 
@@ -64,8 +66,8 @@ class CarDetailView(LoginRequiredMixin, generic.DetailView):
 
 class CarCreateView(LoginRequiredMixin, generic.CreateView):
     model = Car
-    fields = "__all__"
     success_url = reverse_lazy("taxi:car-list")
+    form_class = CarForm
 
 
 class CarUpdateView(LoginRequiredMixin, generic.UpdateView):
@@ -79,6 +81,16 @@ class CarDeleteView(LoginRequiredMixin, generic.DeleteView):
     success_url = reverse_lazy("taxi:car-list")
 
 
+@login_required
+def toggle_car_driver(request, pk):
+    car = get_object_or_404(Car, pk=pk)
+    if car.drivers.filter(pk=request.user.pk).exists():
+        car.drivers.remove(request.user)
+    else:
+        car.drivers.add(request.user)
+    return redirect("taxi:car-detail", pk=pk)
+
+
 class DriverListView(LoginRequiredMixin, generic.ListView):
     model = Driver
     paginate_by = 5
@@ -87,3 +99,18 @@ class DriverListView(LoginRequiredMixin, generic.ListView):
 class DriverDetailView(LoginRequiredMixin, generic.DetailView):
     model = Driver
     queryset = Driver.objects.all().prefetch_related("cars__manufacturer")
+
+
+class DriverCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Driver
+    form_class = DriverCreationForm
+
+
+class DriverDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Driver
+    success_url = reverse_lazy("taxi:driver-list")
+
+
+class DriverUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Driver
+    form_class = DriverUpdateForm
